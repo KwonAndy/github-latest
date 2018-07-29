b19b764f7113f6829d4c970734aa1844cc450b48
Andy <kwona714@hotmail.com>
2018-07-29 16:24:56 -0700

    commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import requests
 if __name__ == "__main__":
     username = sys.argv[1]
 
-    response - requests.get("http:.//api.github.com/users/{}/events".format(username))
+    response = requests.get("http:.//api.github.com/users/{}/events".format(username))
     events = json.loads(response.content)
 
     print(events[0]['created_at'])

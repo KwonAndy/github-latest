d206b6de83e47dc3924c7cbae2ba540f9daa5934
Andy <kwona714@hotmail.com>
2018-07-29 16:24:39 -0700

    commit

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ import requests
 if __name__ == "__main__":
     username = sys.argv[1]
 
+    response - requests.get("http:.//api.github.com/users/{}/events".format(username))
+    events = json.loads(response.content)
+
+    print(events[0]['created_at'])
+
     # TODO:
     #
     # 1. Retrieve a list of "events" associated with the given user name
